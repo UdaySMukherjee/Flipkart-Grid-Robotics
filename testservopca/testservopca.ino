e7fe272a0c1d4c0efbe7b204c731d152c65210eb
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2023-10-01 14:09:49 +0530

    Update testservopca.ino

--- testservopca/testservopca.ino
+++ testservopca/testservopca.ino
@@ -1,39 +1,50 @@
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+#include <HCPCA9685.h>
 
-Adafruit_PWMServoDriver myServo = Adafruit_PWMServoDriver();
+/* Include the HCPCA9685 library */
+#include "HCPCA9685.h"
 
-#define SERVOMIN 150
-#define SERVOMAX 600
+/* I2C slave address for the device/module. For the HCMODU0097 the default I2C address
+   is 0x40 */
+#define  I2CAdd 0x40
 
-uint8_t numberOfServos = 1;
 
-void setup() {
-  Serial.begin(9600);
-  myServo.begin();
-  myServo.setPWMFreq(60);
-  delay(10);
+/* Create an instance of the library */
+HCPCA9685 HCPCA9685(I2CAdd);
+
+
+void setup() 
+{
+  /* Initialise the library and set it to 'servo mode' */ 
+  HCPCA9685.Init(SERVO_MODE);
+
+  /* Wake the device up */
+  HCPCA9685.Sleep(false);
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    int angle = Serial.parseInt();
-    if (angle >= 0 && angle <= 180) {
-      // Map the angle to the servo pulse width
-      uint16_t pulselen = map(angle, 0, 180, SERVOMIN, SERVOMAX);
-      myServo.setPWM(0, 0, pulselen); // Set servo 0 to the specified angle
-      Serial.print("Set servo angle to: ");
-      Serial.println(angle);
-    } else {
-      Serial.println("Invalid angle. Please enter a value between 0 and 180.");
-    }
-  }
 
-  delay(100); // Add a small delay to prevent rapid input reading
+void loop() 
+{
+  unsigned int Pos=400;//10=0,400=180
+
+  /* Sweep the servo back and forth from its minimum to maximum position.
+     If your servo is hitting its end stops then you  should adjust the 
+     values so that the servo can sweep though its full range without hitting
+     the end stops. You can adjust the min & max positions by altering 
+     the trim values in the libraries HCPCA9685.h file*/
+//  for(Pos = 10; Pos < 450; Pos++)
+//  {
+    /* This function sets the servos position. It takes two parameters, 
+     * the first is the servo to control, and the second is the servo 
+     * position. */
+    HCPCA9685.Servo(0, Pos);
+    HCPCA9685.Servo(8, Pos);
+    delay(10);
+//  }
+  
+//  for(Pos = 450; Pos >= 10; Pos--)
+//  {
+    HCPCA9685.Servo(0, Pos);
+     HCPCA9685.Servo(8, Pos);
+    delay(10);
+//  }
 }
-
-//servo 0 = 0-180
-//servo 1 = 0-unknown
-//servo 2 = 0-120
-//servo 3 = 0-180
-//servoo 4 = 0-180
